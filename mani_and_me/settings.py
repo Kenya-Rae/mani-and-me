2edfc190ce58ec8651104664f7382d77676929af
Kenya Francis <kenyarae99@gmail.com>
2025-01-15 11:48:23 +0000

    Fix set up on allauth

--- mani_and_me/settings.py
+++ mani_and_me/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'mani_and_me.urls'

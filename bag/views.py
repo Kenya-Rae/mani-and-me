30d1916184010c37357b903d833d672dc0ec37f4
Kenya Francis <kenyarae99@gmail.com>
2025-01-20 14:31:10 +0000

    Add adjust/remove from bag views include inventory updates, calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 from products.models import Inventory
 
@@ -9,8 +9,10 @@ def view_shopping_bag(request):
 
     return render (request, 'bag/shopping_bag.html')
 
+
 def add_to_bag(request, item_id):
-    """ Add a quantity of an item to the shopping bag """
+    """ Add a quantity of an item to the shopping bag 
+        - Code from Boutique Ado with adaptations """
 
     size = request.POST.get('size')  # Get size
     quantity = int(request.POST.get('quantity'))
@@ -48,3 +50,112 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_shopping_bag(request, item_id):
+    """ Adjust item quantity in the shopping bag 
+        - Code from Boutique Ado with adaptations """
+    
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    # Retrieve current quantity in the shopping bag
+    current_quantity = 0
+    if size:
+        current_quantity = bag.get(item_id, {}).get('items_by_size', {}).get(size, 0)
+    else:
+        current_quantity = bag.get(item_id, 0)
+
+    if size:
+        if quantity > 0:
+            # Update inventory if increasing quantity
+            inventory_item = Inventory.objects.get(product_id=item_id, size=size)
+            if quantity > current_quantity:
+                if inventory_item.quantity >= (quantity - current_quantity):
+                    inventory_item.quantity -= (quantity - current_quantity)  # Deduct the difference
+                    inventory_item.save()
+                else:
+                    messages.error(request, "Not enough stock available.")
+                    return redirect(reverse('view_shopping_bag'))
+            elif quantity < current_quantity:
+                inventory_item.quantity += (current_quantity - quantity)  # Restore the difference
+                inventory_item.save()
+            
+            # Update the bag quantity
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # Remove item and return stock to inventory
+            inventory_item = Inventory.objects.get(product_id=item_id, size=size)
+            inventory_item.quantity += current_quantity  # Return the entire quantity to inventory
+            inventory_item.save()
+            
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            # Update inventory if increasing quantity
+            inventory_item = Inventory.objects.get(product_id=item_id)
+            if quantity > current_quantity:
+                if inventory_item.quantity >= (quantity - current_quantity):
+                    inventory_item.quantity -= (quantity - current_quantity)  # Deduct the difference
+                    inventory_item.save()
+                else:
+                    messages.error(request, "Not enough stock available.")
+                    return redirect(reverse('view_shopping_bag'))
+            elif quantity < current_quantity:
+                inventory_item.quantity += (current_quantity - quantity)  # Restore the difference
+                inventory_item.save()
+            
+            # Update the bag quantity
+            bag[item_id] = quantity
+        else:
+            # Remove item and return stock to inventory
+            inventory_item = Inventory.objects.get(product_id=item_id)
+            inventory_item.quantity += current_quantity  # Return the entire quantity to inventory
+            inventory_item.save()
+            
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_shopping_bag'))
+
+
+def remove_from_shopping_bag(request, item_id):
+    """ Remove item quantity from the shopping bag and update inventory """
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            current_quantity = bag.get(item_id, {}).get('items_by_size', {}).get(size, 0)
+            if current_quantity > 0:
+                # Update inventory to add back the removed quantity
+                inventory_item = Inventory.objects.get(product_id=item_id, size=size)
+                inventory_item.quantity += current_quantity
+                inventory_item.save()
+
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            current_quantity = bag.get(item_id, 0)
+            if current_quantity > 0:
+                # Update inventory to add back the removed quantity
+                inventory_item = Inventory.objects.get(product_id=item_id)
+                inventory_item.quantity += current_quantity
+                inventory_item.save()
+
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)

23bd546a0e80f0d6abc204b7f9643855d31a4298
Kenya Francis <kenyarae99@gmail.com>
2025-01-28 11:45:13 +0000

    Add contact and about us urls and template

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('contact-us/', views.contact_us, name='contact_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,11 @@ def index(request):
     """ View to return homepage """
 
     return render (request, 'home/index.html')
+
+
+def about_us(request):
+    return render(request, 'home/about_us.html')
+
+
+def contact_us(request):
+    return render(request, 'home/contact_us.html')

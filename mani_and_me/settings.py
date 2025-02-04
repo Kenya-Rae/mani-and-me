9379f454de2901e3181c2cf55bbfba983d4c7b9d
Kenya Francis <kenyarae99@gmail.com>
2025-02-04 12:49:49 +0000

    Set up AWS

--- mani_and_me/settings.py
+++ mani_and_me/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
     # Other apps i.e Crispy Forms
     'crispy_forms',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -177,6 +178,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket configuration
+    AWS_STORAGE_BUCKET_NAME = 'mani-and-me'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Shopping bag (Delivery Threshold & Deals)
 FREE_DELIVERY_THRESHOLD = 50

38a41f242590edbca70a8264602e3e32751858e1
Kenya Francis <kenyarae99@gmail.com>
2025-02-04 10:30:48 +0000

    Deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mani_and_me.wsgi:application
--- mani_and_me/settings.py
+++ mani_and_me/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-dir9k1$%c5!@9!@s$jw0$=m&v#ejz@y013gntp^sak^frcj)2i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-kenyarae-maniandme-b8erhd4dxvy.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-kenyarae-maniandme-b8erhd4dxvy.ws.codeinstitute-ide.net','localhost','https://mani-and-me-3a77440a82f2.herokuapp.com/']
 
 
 # Application definition
@@ -110,7 +111,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -122,12 +123,17 @@ WSGI_APPLICATION = 'mani_and_me.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation

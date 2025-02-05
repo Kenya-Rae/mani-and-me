90382786b87ee0848ef7bbe5f43f529d96c27b04
Kenya Francis <kenyarae99@gmail.com>
2025-02-05 19:08:15 +0000

    Changed DUBG setting back to environ

--- mani_and_me/settings.py
+++ mani_and_me/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 ALLOWED_HOSTS = [
     '8000-kenyarae-maniandme-b8erhd4dxvy.ws.codeinstitute-ide.net',
     'localhost', 'mani-and-me-3a77440a82f2.herokuapp.com', '.herokuapp.com']
